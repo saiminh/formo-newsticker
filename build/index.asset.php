6f203b7b45017a9ccbb1c624ac07967b70a93d31
saiminh <saiminh@gmail.com>
2024-09-09 22:59:11 +0200

    update version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3e4f129876dbd30583ec');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1bf2716131fa68ee8a29');
